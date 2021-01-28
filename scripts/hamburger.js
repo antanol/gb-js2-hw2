c5837e4a746fb187313325d7411aa7fff9e5bfa5
Ann Antonova <ms.antanol@yandex.ru>
2021-01-28 03:03:39 +0300

    прописала методы класса для изменения состава бургера

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -10,29 +10,82 @@ for (let box of checkboxes){
 function checkBoxing(evt){
     for (let elem of checkedElems){
         elem.style.backgroundColor = "transparent";
-        if (elem.dataset.name == evt.target.value){
+        if (elem.children[0].checked){
             elem.style.backgroundColor = "#eafbc8";
         }
-        console.log(elem.dataset.name == evt.target.value);
     }
 }
 
 class Burger{
-    constructor(structure=[]){
-        this.structure = structure;
+    constructor(size, topping, spice, price=0, ccal=0){
+        this.size = size;
+        this.topping = topping;
+        this.spice = spice;
+
+        this.price = price;
+        this.ccal = ccal;
     }
 
-    editElems(elem, price, ccal){
-        this.structure.pop(elem);
-        countPrice()
+    editSize(evt){
+        if(evt.target.value != this.size){
+            if (this.size){
+                this.countPrice(-document.querySelector(`label[data-name='${this.size}'] .price`).innerText.split(' ')[0]);
+                this.countCcal(-document.querySelector(`label[data-name='${this.size}'] .params`).innerText.split(' ')[0]);
+            }
+            this.size = evt.target.value;
+            this.countPrice(document.querySelector(`label[data-name='${evt.target.value}'] .price`).innerText.split(' ')[0]);
+            this.countCcal(document.querySelector(`label[data-name='${evt.target.value}'] .params`).innerText.split(' ')[0]);
+        };
     }
 
-    countPrice(){
-
+    editTopping(evt){
+        if(evt.target.value != this.topping){
+            if (this.topping){
+                this.countPrice(-document.querySelector(`label[data-name='${this.topping}'] .price`).innerText.split(' ')[0]);
+                this.countCcal(-document.querySelector(`label[data-name='${this.topping}'] .params`).innerText.split(' ')[0]);
+            }
+            this.topping = evt.target.value;
+            this.countPrice(document.querySelector(`label[data-name='${evt.target.value}'] .price`).innerText.split(' ')[0]);
+            this.countCcal(document.querySelector(`label[data-name='${evt.target.value}'] .params`).innerText.split(' ')[0]);
+        };
     }
 
-    countCcal(){
+    editSpices(evt){
+        if(evt.target.value != this.spice){
+            if (this.spice){
+                this.countPrice(-document.querySelector(`label[data-name='${this.spice}'] .price`).innerText.split(' ')[0]);
+                this.countCcal(-document.querySelector(`label[data-name='${this.spice}'] .params`).innerText.split(' ')[0]);
+            }
+            this.spice = evt.target.value;
+            this.countPrice(document.querySelector(`label[data-name='${evt.target.value}'] .price`).innerText.split(' ')[0]);
+            this.countCcal(document.querySelector(`label[data-name='${evt.target.value}'] .params`).innerText.split(' ')[0]);
+        };
+    }
 
+    countPrice(elemPrice){
+        this.price += Number(elemPrice);
+        document.querySelector('.total-price').innerText = this.price;
+    }
+
+    countCcal(elemCcal){
+        this.ccal += Number(elemCcal);
+        document.querySelector('.total-ccal').innerText = this.ccal;
     }
 }
 
+let burger = new Burger();
+let choiceSize = document.querySelectorAll("input[name='size-burger']");
+let choiceToppings = document.querySelectorAll("input[name='topping']");
+let choiceSpices = document.querySelectorAll("input[name='spices']")
+
+for (let size of choiceSize){
+    size.addEventListener('change', burger.editSize.bind(burger));
+}
+
+for (let topping of choiceToppings){
+    topping.addEventListener('change', burger.editTopping.bind(burger));
+}
+
+for (let spice of choiceSpices){
+    spice.addEventListener('change', burger.editSpices.bind(burger));
+}
